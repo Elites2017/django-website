1357dec16a7eeffd7651b9555df5394fffe50c52
victor-iyiola <javafolabi@gmail.com>
2017-09-09 13:59:54 +0100

    Made migrations

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from . import views
+from django.views.generic import ListView
+from blog.models import Post
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/index.html")),
 ]
